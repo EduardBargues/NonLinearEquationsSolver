de62ee76c592915aa8b71f64da46a41a6eec6dba
epsi1on <ehsan.ma@gmx.com>
2020-08-09 10:42:01 +0430

    Update ILinearSolver.cs
    Add methods and properties, same as this file:
    
    https://github.com/BriefFiniteElementNet/BriefFiniteElement.Net/blob/master/BriefFiniteElementNet.Common/ISolver.cs

--- NLES/ILinearSolver.cs
+++ NLES/ILinearSolver.cs
@@ -2,6 +2,9 @@
 
 namespace NLES
 {
+    /// <summary>
+    /// Represents an interface for a solver to solve A*x = b.
+    /// </summary>
     public interface ILinearSolver
     {
         /// <summary>
@@ -10,5 +13,29 @@ namespace NLES
         /// <param name="input"></param>
         /// <returns></returns>
         Vector<double> Solve(Vector<double> input);
+        
+        
+        /// Gets a value indicating whether the solver is initialized.
+        /// </summary>
+        bool IsInitialized { get; }
+
+        /// <summary>
+        /// Gets the type of the solver.
+        /// </summary>
+        //BuiltInSolverType SolverType { get; }
+
+        /// <summary>
+        /// Initializes the solver regarding <see cref="A" /> matrix.
+        /// </summary>
+        void Initialize();
+
+        /// <summary>
+        /// Solves a the system of A*x=b and store the x in <see cref="x" />.
+        /// </summary>
+        /// <param name="b">Right hand side vector.</param>
+        /// <param name="x">Solution vector.</param>
+        void Solve(double[] b, double[] x);
     }
+    
+    
 }
