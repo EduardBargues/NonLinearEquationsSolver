aa7dfdfa0c0168c7fd8c11b03618ec4ab851168b
Eduard <eduardbargues@gmail.com>
2020-08-09 20:54:20 +0200

    remove extra properties and methods from ILinearSolver.cs

--- NLES/ILinearSolver.cs
+++ NLES/ILinearSolver.cs
@@ -13,23 +13,5 @@ namespace NLES
         /// <param name="input"></param>
         /// <returns></returns>
         Vector<double> Solve(Vector<double> input);
-             
-        /// Gets a value indicating whether the solver is initialized.
-        /// </summary>
-        bool IsInitialized { get; }
-
-        /// <summary>
-        /// Initializes the solver regarding <see cref="A" /> matrix.
-        /// </summary>
-        void Initialize();
-
-        /// <summary>
-        /// Solves a the system of A*x=b and store the x in <see cref="x" />.
-        /// </summary>
-        /// <param name="b">Right hand side vector.</param>
-        /// <param name="x">Solution vector.</param>
-        void Solve(double[] b, double[] x);
     }
-    
-    
 }
